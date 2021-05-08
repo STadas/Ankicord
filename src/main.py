7abd8a338512f3fa5d28902a312a4302d60342f3
TSalten <saltenis.tadas@gmail.com>
2021-05-08 17:18:56 +0300

    made rpc update its own thread
    #5

--- src/main.py
+++ src/main.py
@@ -4,8 +4,11 @@
 import time
 import os
 from typing import Union
+from threading import Thread, Semaphore
+
 from anki.hooks import addHook
 from aqt import mw
+
 from .pypresence import presence as pp
 
 
@@ -16,27 +19,24 @@ class Ankicord():
         config = self.__get_resolved_config()
         self.main_conf = config['main']
         self.status_conf = config['statuses']
-        print(config)
 
-        self.due_message = "   "
+        self.rpc_next_details = "   "
+        self.rpc_next_state = "   "
         self.deck_name = ""
         self.skip_edit = False
 
-        self.start_time = round(time.time())
-        self.curr_time = round(time.time()) - 15
-
         self.connected = False
         self.rpc = pp.Presence('745326655395856514')
-
-        if not self.__get_config_val(self.main_conf, 'timer', bool):
-            self.start_time = None
+        self.start_time = round(time.time())
 
         if self.__get_config_val(self.main_conf, 'activity', bool):
-            self.__rpc_update(self.__get_config_val(self.status_conf,
-                                                    'menu_status',
-                                                    str))
+            self.rpc_next_details = self.__get_config_val(self.status_conf,
+                                                          'menu_status',
+                                                          str)
         else:
-            self.__rpc_update("   ")
+            self.rpc_next_details = "   "
+
+        self.sem = Semaphore()
 
     def __get_resolved_config(self,
                               cfg: Union[dict, list] = None) -> Union[dict, list]:
@@ -63,7 +63,7 @@ class Ankicord():
                 cfg[k] = table[cfg[k]]
         return cfg
 
-    def connect_rpc(self):
+    def connect_rpc(self) -> None:
         """Connect to the Discord Rich Presence"""
         try:
             self.rpc.connect()
@@ -104,51 +104,47 @@ class Ankicord():
             print(ex)
             return None
 
-    def __get_config_val(self, cfg, cfg_key, cfg_type):
+    def __get_config_val(self, cfg: Union[list, dict], cfg_key: str, cfg_type):
         """Check if key in config exists. If it does, get value, if not - None"""
         cfg_val = cfg.get(cfg_key, None)
-        print(cfg_key + ":", cfg_val)
         if isinstance(cfg_val, cfg_type):
             return cfg_val
         return None
 
-    def __rpc_update(self, details_message) -> None:
+    def __rpc_update(self) -> None:
         """Updates the Discord Rich Presence with provided details_message"""
+        self.sem.acquire()
         try:
             if not self.connected:
                 self.connect_rpc()
 
             # update Rich Presence
-            if round(time.time()) - 15 > self.curr_time:
-                #! Spotify (LINUX ONLY)
-                if self.__get_config_val(self.main_conf, 'spotify', bool):
-                    spotify_info = self.__get_spotify_info()
-                    if spotify_info is not None:
-                        self.rpc.update(details=details_message,
-                                        state=self.due_message,
-                                        large_image="anki",
-                                        small_image="spotify",
-                                        small_text=spotify_info,
-                                        start=self.start_time)
-                    else:
-                        self.rpc.update(details=details_message,
-                                        state=self.due_message,
-                                        large_image="anki",
-                                        start=self.start_time)
+            #! Spotify (LINUX ONLY)
+            if self.__get_config_val(self.main_conf, 'spotify', bool):
+                spotify_info = self.__get_spotify_info()
+                if spotify_info is not None:
+                    self.rpc.update(details=self.rpc_next_details,
+                                    state=self.rpc_next_state,
+                                    large_image="anki",
+                                    small_image="spotify",
+                                    small_text=spotify_info,
+                                    start=self.start_time)
                 else:
-                    print("details", f"'{str(details_message)}'")
-                    print("due", f"'{str(self.due_message)}'")
-                    print("start", self.start_time)
-                    self.rpc.update(details=details_message,
-                                    state=self.due_message,
+                    self.rpc.update(details=self.rpc_next_details,
+                                    state=self.rpc_next_state,
                                     large_image="anki",
                                     start=self.start_time)
-                self.curr_time = round(time.time())
+            else:
+                self.rpc.update(details=self.rpc_next_details,
+                                state=self.rpc_next_state,
+                                large_image="anki",
+                                start=self.start_time)
         except Exception as ex:
             self.connected = False
             print(ex)
+        self.sem.release()
 
-    def update_due_message(self) -> None:
+    def __update_rpc_next_state(self) -> None:
         """Calculate reviews due"""
         due_count = 0
 
@@ -158,48 +154,52 @@ class Ankicord():
 
         # Correct for single or no cards
         if due_count == 0:
-            self.due_message = self.__get_config_val(self.status_conf,
-                                                     'no_cards_left_txt',
-                                                     str)
+            self.rpc_next_state = self.__get_config_val(self.status_conf,
+                                                        'no_cards_left_txt',
+                                                        str)
         elif due_count == 1:
-            self.due_message = "(" + str(due_count) + " card left)"
+            self.rpc_next_state = "(" + str(due_count) + " card left)"
         else:
-            self.due_message = "(" + str(due_count) + " cards left)"
+            self.rpc_next_state = "(" + str(due_count) + " cards left)"
 
     def on_state(self, state, _old_state):
-        """Take current state and old_state from hook. If browsing, skip
-        'edit' hook. Call update"""
+        """Take current state and old_state from hook; If browsing, skip
+        'edit' hook; Call update"""
+        self.sem.acquire()
         if self.__get_config_val(self.main_conf, 'card_count', bool):
-            self.update_due_message()
+            self.__update_rpc_next_state()
 
         if not self.__get_config_val(self.main_conf, 'activity', bool):
-            self.__rpc_update("   ")
+            self.rpc_next_details = "   "
             return
 
         if state == "deckBrowser":
-            self.__rpc_update(self.__get_config_val(self.status_conf,
-                                                    'menu_status',
-                                                    str))
+            self.rpc_next_details = self.__get_config_val(self.status_conf,
+                                                          'menu_status',
+                                                          str)
 
         elif state == "review":
             reviews_msg = self.__get_config_val(self.status_conf,
                                                 'reviewing_status',
                                                 str)
-            show_deck = self.__get_config_val(self.main_conf, 'deck_name', bool)
+            show_deck = self.__get_config_val(self.main_conf,
+                                              'deck_name',
+                                              bool)
             if show_deck and self.deck_name != "":
                 reviews_msg += " [" + self.deck_name + "]"
-            self.__rpc_update(reviews_msg)
+            self.rpc_next_details = reviews_msg
 
         elif state == "browse":
             self.skip_edit = True
-            self.__rpc_update(self.__get_config_val(self.status_conf,
-                                                    'browsing_status',
-                                                    str))
+            self.rpc_next_details = self.__get_config_val(self.status_conf,
+                                                          'browsing_status',
+                                                          str)
 
         elif state == "edit":
-            self.__rpc_update(self.__get_config_val(self.status_conf,
-                                                    'editing_status',
-                                                    str))
+            self.rpc_next_details = self.__get_config_val(self.status_conf,
+                                                          'editing_status',
+                                                          str)
+        self.sem.release()
 
     def on_browse(self, _x):
         """Handle browse state"""
@@ -217,9 +217,17 @@ class Ankicord():
         self.deck_name = str(mw.col.decks.get(mw.reviewer.card.did)['name'])
         self.on_state("review", "dummy")
 
+    def job(self):
+        """Loop: call rpc update and wait 15 seconds."""
+        while True:
+            self.__rpc_update()
+            time.sleep(15)
+
 
 ac = Ankicord()
 ac.connect_rpc()
+t = Thread(target=ac.job, daemon=True)
+t.start()
 
 addHook("afterStateChange", ac.on_state)
 addHook("browser.setupMenus", ac.on_browse)
